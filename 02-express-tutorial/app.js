5481040707c0449d69feabf5ad9801584e78631e
Nigel Diones <ndiones14@gmail.com>
2022-07-04 21:53:14 +0800

    Api example

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,27 +1,53 @@
 const express = require('express')
-const path = require('path')
 const app = express()
-
-app.use(express.static('./public'))
-//
-//app.get('/',(req,res)=>{
-//    res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
-    // adding to static assests
-    // SSR
-//})
-//
-app.all('*',(req,res)=>{
-    res.status(404).send('resource not found')
+const {products} = require('./data.js')
+app.get('/',(req,res)=>{
+    res.send('<h1>Home Page</h1><a href="/api/products">products<a>')
 })
 
-app.listen(5000,(req,res)=>{
-    console.log('server is listening on port 5000')
+app.get('/api/products',(req, res) =>{
+    const newProducts = products.map((product) =>{
+        const {id,name,image} = product
+        return {id,name,image}
+    })
+    res.json(newProducts)
 })
 
-// app.get
-// app.post
-// app.put
-// app.delete
-// app.all
-// app.use
-// app.listen
+app.get('/api/products/:productID',(req, res) =>{
+ //   console.log(req)
+ //   console.log(req.params)
+    const {productID} = req.params
+    const singleProduct = products.find((product) => product.id === Number(productID))
+    if(!singleProduct){
+        return res.status(404).send('Product Does Not Exist')
+    }
+    console.log(singleProduct)
+    return res.json(singleProduct)
+})
+app.get('/api/products/:productID/reviews/:reviewID',(req, res)=>{
+    console.log(req.params)
+    res.send('Hello World')
+})
+
+app.get('/api/v1/query',(req, res) =>{
+   // console.log(req.query)
+    const{search,limit} = req.query
+    let sortedProducts = [...products]
+    if(search){
+        sortedProducts = sortedProducts.filter((product) =>{
+            return product.name.startsWith(search)
+        })
+    }
+    if(limit){
+        sortedProducts = sortedProducts.slice(0,Number(limit))
+    }
+    if(sortedProducts.length <1){
+       // res.status(200).send('no products matched your search')
+       return res.status(200).json({sucess: true,data: []})
+    }
+    return res.status(200).json(sortedProducts)
+})
+
+app.listen(5000, () =>{
+    console.log('Server is listening on port 5000...')
+})
